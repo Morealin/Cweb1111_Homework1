afa6bf2103fd6a78e304247cf54f9b9fa632ecf8
John Dirks <dirjohn@dunwoody.edu>
2017-09-15 09:41:45 -0500

    Lab1_2017-09-15

---
+++ GearScoreData.js
@@ -0,0 +1,1038 @@
+gearScore_Array = 
+	[
+	 {
+	   "Name": "Smack_daddy",
+	   "Class": "Berserker",
+	   "Level": 56,
+	   "AAP": 90,
+	   "AP": 95,
+	   "DP": 206,
+	   "GS": 301
+	 },
+	 {
+	   "Name": "Cujo",
+	   "Class": "Berserker",
+	   "Level": 59,
+	   "AAP": 155,
+	   "AP": 161,
+	   "DP": 300,
+	   "GS": 461
+	 },
+	 {
+	   "Name": "Etraks",
+	   "Class": "Berserker",
+	   "Level": 60,
+	   "AAP": 190,
+	   "AP": 180,
+	   "DP": 291,
+	   "GS": 481
+	 },
+	 {
+	   "Name": "Damboi",
+	   "Class": "Berserker",
+	   "Level": 59,
+	   "AAP": 169,
+	   "AP": 175,
+	   "DP": 278,
+	   "GS": 453
+	 },
+	 {
+	   "Name": "Rooee",
+	   "Class": "Berserker",
+	   "Level": 59,
+	   "AAP": 168,
+	   "AP": 174,
+	   "DP": 287,
+	   "GS": 461
+	 },
+	 {
+	   "Name": "BackGrabs",
+	   "Class": "Berserker",
+	   "Level": 60,
+	   "AAP": 181,
+	   "AP": 181,
+	   "DP": 291,
+	   "GS": 472
+	 },
+	 {
+	   "Name": "TopSon",
+	   "Class": "Berserker",
+	   "Level": 60,
+	   "AAP": 204,
+	   "AP": 210,
+	   "DP": 296,
+	   "GS": 506
+	 },
+	 {
+	   "Name": "Hate_Machine",
+	   "Class": "Berserker",
+	   "Level": 60,
+	   "AAP": 183,
+	   "AP": 173,
+	   "DP": 314,
+	   "GS": 497
+	 },
+	 {
+	   "Name": "Naealef",
+	   "Class": "Maewha",
+	   "Level": 59,
+	   "AAP": 186,
+	   "AP": 204,
+	   "DP": 255,
+	   "GS": 459
+	 },
+	 {
+	   "Name": "Ksayssa",
+	   "Class": "DK",
+	   "Level": 56,
+	   "AAP": 171,
+	   "AP": 189,
+	   "DP": 221,
+	   "GS": 410
+	 },
+	 {
+	   "Name": "Amylita",
+	   "Class": "DK",
+	   "Level": 56,
+	   "AAP": 139,
+	   "AP": 151,
+	   "DP": 220,
+	   "GS": 371
+	 },
+	 {
+	   "Name": "HaiToe",
+	   "Class": "DK",
+	   "Level": 56,
+	   "AAP": 107,
+	   "AP": 102,
+	   "DP": 194,
+	   "GS": 301
+	 },
+	 {
+	   "Name": "KnightofFaps",
+	   "Class": "DK",
+	   "Level": 60,
+	   "AAP": 214,
+	   "AP": 212,
+	   "DP": 291,
+	   "GS": 505
+	 },
+	 {
+	   "Name": "Hugzm",
+	   "Class": "DK",
+	   "Level": 60,
+	   "AAP": 180,
+	   "AP": 186,
+	   "DP": 280,
+	   "GS": 466
+	 },
+	 {
+	   "Name": "Weheh",
+	   "Class": "DK",
+	   "Level": 60,
+	   "AAP": 238,
+	   "AP": 236,
+	   "DP": 263,
+	   "GS": 501
+	 },
+	 {
+	   "Name": "Aladaryia",
+	   "Class": "DK",
+	   "Level": 60,
+	   "AAP": 212,
+	   "AP": 212,
+	   "DP": 283,
+	   "GS": 495
+	 },
+	 {
+	   "Name": "DarkHamster",
+	   "Class": "DK",
+	   "Level": 60,
+	   "AAP": 213,
+	   "AP": 211,
+	   "DP": 283,
+	   "GS": 496
+	 },
+	 {
+	   "Name": "JuOnDaSexyGrudge",
+	   "Class": "DK",
+	   "Level": 59,
+	   "AAP": 211,
+	   "AP": 209,
+	   "DP": 263,
+	   "GS": 474
+	 },
+	 {
+	   "Name": "Kaijoichi",
+	   "Class": "Kunoichi",
+	   "Level": 59,
+	   "AAP": 204,
+	   "AP": 202,
+	   "DP": 263,
+	   "GS": 467
+	 },
+	 {
+	   "Name": "RocketRaccoon",
+	   "Class": "Kunoichi",
+	   "Level": 59,
+	   "AAP": 192,
+	   "AP": 182,
+	   "DP": 289,
+	   "GS": 481
+	 },
+	 {
+	   "Name": "Rexaere",
+	   "Class": "Kunoichi",
+	   "Level": 59,
+	   "AAP": 219,
+	   "AP": 197,
+	   "DP": 283,
+	   "GS": 502
+	 },
+	 {
+	   "Name": "Nayashe",
+	   "Class": "Maewha",
+	   "Level": 60,
+	   "AAP": 228,
+	   "AP": 226,
+	   "DP": 263,
+	   "GS": 491
+	 },
+	 {
+	   "Name": "Deathxlotus",
+	   "Class": "Maewha",
+	   "Level": 61,
+	   "AAP": 231,
+	   "AP": 213,
+	   "DP": 283,
+	   "GS": 514
+	 },
+	 {
+	   "Name": "Abztracta",
+	   "Class": "Maewha",
+	   "Level": 59,
+	   "AAP": 200,
+	   "AP": 190,
+	   "DP": 255,
+	   "GS": 455
+	 },
+	 {
+	   "Name": "Mythrius",
+	   "Class": "Musa",
+	   "Level": 58,
+	   "AAP": 214,
+	   "AP": 204,
+	   "DP": 263,
+	   "GS": 477
+	 },
+	 {
+	   "Name": "Sirges",
+	   "Class": "Ninja",
+	   "Level": 58,
+	   "AAP": 196,
+	   "AP": 202,
+	   "DP": 265,
+	   "GS": 467
+	 },
+	 {
+	   "Name": "Suangi",
+	   "Class": "Ninja",
+	   "Level": 60,
+	   "AAP": 195,
+	   "AP": 190,
+	   "DP": 304,
+	   "GS": 499
+	 },
+	 {
+	   "Name": "Haze_Haya",
+	   "Class": "Ranger",
+	   "Level": 56,
+	   "AAP": 122,
+	   "AP": 130,
+	   "DP": 199,
+	   "GS": 329
+	 },
+	 {
+	   "Name": "Aliciada",
+	   "Class": "Ranger",
+	   "Level": 56,
+	   "AAP": 179,
+	   "AP": 173,
+	   "DP": 176,
+	   "GS": 355
+	 },
+	 {
+	   "Name": "Shocky",
+	   "Class": "Ranger",
+	   "Level": 59,
+	   "AAP": 218,
+	   "AP": 216,
+	   "DP": 263,
+	   "GS": 481
+	 },
+	 {
+	   "Name": "SenpaiYamete",
+	   "Class": "Ranger",
+	   "Level": 61,
+	   "AAP": 241,
+	   "AP": 239,
+	   "DP": 283,
+	   "GS": 524
+	 },
+	 {
+	   "Name": "Lyril",
+	   "Class": "Ranger",
+	   "Level": 60,
+	   "AAP": 249,
+	   "AP": 247,
+	   "DP": 239,
+	   "GS": 488
+	 },
+	 {
+	   "Name": "Zondetta",
+	   "Class": "Sorceress",
+	   "Level": 57,
+	   "AAP": 177,
+	   "AP": 175,
+	   "DP": 277,
+	   "GS": 454
+	 },
+	 {
+	   "Name": "DelicateBoo",
+	   "Class": "Sorceress",
+	   "Level": 59,
+	   "AAP": 198,
+	   "AP": 196,
+	   "DP": 271,
+	   "GS": 469
+	 },
+	 {
+	   "Name": "Pizzelle",
+	   "Class": "Sorceress",
+	   "Level": 60,
+	   "AAP": 202,
+	   "AP": 200,
+	   "DP": 257,
+	   "GS": 459
+	 },
+	 {
+	   "Name": "Houngwee",
+	   "Class": "Sorceress",
+	   "Level": 59,
+	   "AAP": 219,
+	   "AP": 120,
+	   "DP": 266,
+	   "GS": 485
+	 },
+	 {
+	   "Name": "Gyenn",
+	   "Class": "Sorceress",
+	   "Level": 60,
+	   "AAP": 239,
+	   "AP": 237,
+	   "DP": 294,
+	   "GS": 533
+	 },
+	 {
+	   "Name": "Joltyx",
+	   "Class": "Sorceress",
+	   "Level": 60,
+	   "AAP": 198,
+	   "AP": 196,
+	   "DP": 242,
+	   "GS": 440
+	 },
+	 {
+	   "Name": "Lilith_Storm",
+	   "Class": "Sorceress",
+	   "Level": 60,
+	   "AAP": 202,
+	   "AP": 200,
+	   "DP": 283,
+	   "GS": 485
+	 },
+	 {
+	   "Name": "Blub",
+	   "Class": "Sorceress",
+	   "Level": 60,
+	   "AAP": 223,
+	   "AP": 221,
+	   "DP": 273,
+	   "GS": 496
+	 },
+	 {
+	   "Name": "Kalamete",
+	   "Class": "Sorceress",
+	   "Level": 59,
+	   "AAP": 215,
+	   "AP": 185,
+	   "DP": 275,
+	   "GS": 490
+	 },
+	 {
+	   "Name": "Wolfmancer",
+	   "Class": "Sorceress",
+	   "Level": 60,
+	   "AAP": 210,
+	   "AP": 208,
+	   "DP": 266,
+	   "GS": 476
+	 },
+	 {
+	   "Name": "Cinerum",
+	   "Class": "Striker",
+	   "Level": 58,
+	   "AAP": 173,
+	   "AP": 186,
+	   "DP": 290,
+	   "GS": 476
+	 },
+	 {
+	   "Name": "Devilzz",
+	   "Class": "Striker",
+	   "Level": 59,
+	   "AAP": 200,
+	   "AP": 198,
+	   "DP": 278,
+	   "GS": 478
+	 },
+	 {
+	   "Name": "Xinaku",
+	   "Class": "Striker",
+	   "Level": 58,
+	   "AAP": 136,
+	   "AP": 162,
+	   "DP": 271,
+	   "GS": 433
+	 },
+	 {
+	   "Name": "Dana_Prajna",
+	   "Class": "Tamer",
+	   "Level": 56,
+	   "AAP": 138,
+	   "AP": 144,
+	   "DP": 178,
+	   "GS": 322
+	 },
+	 {
+	   "Name": "EmeraldMononoke",
+	   "Class": "Tamer",
+	   "Level": 56,
+	   "AAP": 132,
+	   "AP": 140,
+	   "DP": 167,
+	   "GS": 307
+	 },
+	 {
+	   "Name": "Valkyries",
+	   "Class": "Tamer",
+	   "Level": 60,
+	   "AAP": 232,
+	   "AP": 230,
+	   "DP": 298,
+	   "GS": 530
+	 },
+	 {
+	   "Name": "Yengz",
+	   "Class": "Tamer",
+	   "Level": 60,
+	   "AAP": 229,
+	   "AP": 227,
+	   "DP": 275,
+	   "GS": 504
+	 },
+	 {
+	   "Name": "Zymore",
+	   "Class": "Valkyrie",
+	   "Level": 56,
+	   "AAP": 148,
+	   "AP": 142,
+	   "DP": 253,
+	   "GS": 401
+	 },
+	 {
+	   "Name": "Skytee",
+	   "Class": "Valkyrie",
+	   "Level": 61,
+	   "AAP": 211,
+	   "AP": 209,
+	   "DP": 323,
+	   "GS": 534
+	 },
+	 {
+	   "Name": "DavinSngel",
+	   "Class": "Valkyrie",
+	   "Level": 60,
+	   "AAP": 201,
+	   "AP": 199,
+	   "DP": 283,
+	   "GS": 484
+	 },
+	 {
+	   "Name": "FineHeart",
+	   "Class": "Valkyrie",
+	   "Level": 60,
+	   "AAP": 199,
+	   "AP": 189,
+	   "DP": 294,
+	   "GS": 493
+	 },
+	 {
+	   "Name": "BiaBellona",
+	   "Class": "Valkyrie",
+	   "Level": 59,
+	   "AAP": 217,
+	   "AP": 207,
+	   "DP": 263,
+	   "GS": 480
+	 },
+	 {
+	   "Name": "Pixcess",
+	   "Class": "Valkyrie",
+	   "Level": 60,
+	   "AAP": 217,
+	   "AP": 195,
+	   "DP": 292,
+	   "GS": 509
+	 },
+	 {
+	   "Name": "BlackIcelisted",
+	   "Class": "Valkyrie",
+	   "Level": 59,
+	   "AAP": 216,
+	   "AP": 206,
+	   "DP": 283,
+	   "GS": 499
+	 },
+	 {
+	   "Name": "Iris_Aurillius",
+	   "Class": "Valkyrie",
+	   "Level": 59,
+	   "AAP": 178,
+	   "AP": 184,
+	   "DP": 274,
+	   "GS": 458
+	 },
+	 {
+	   "Name": "Xedi",
+	   "Class": "Valkyrie",
+	   "Level": 58,
+	   "AAP": 151,
+	   "AP": 157,
+	   "DP": 323,
+	   "GS": 480
+	 },
+	 {
+	   "Name": "Altair",
+	   "Class": "Warrior",
+	   "Level": 60,
+	   "AAP": 231,
+	   "AP": 221,
+	   "DP": 263,
+	   "GS": 494
+	 },
+	 {
+	   "Name": "SenpaiYui",
+	   "Class": "Warrior",
+	   "Level": 56,
+	   "AAP": 137,
+	   "AP": 130,
+	   "DP": 260,
+	   "GS": 397
+	 },
+	 {
+	   "Name": "Kantotero",
+	   "Class": "Warrior",
+	   "Level": 57,
+	   "AAP": 119,
+	   "AP": 123,
+	   "DP": 245,
+	   "GS": 368
+	 },
+	 {
+	   "Name": "FatherFist",
+	   "Class": "Warrior",
+	   "Level": 60,
+	   "AAP": 200,
+	   "AP": 198,
+	   "DP": 302,
+	   "GS": 502
+	 },
+	 {
+	   "Name": "JRock",
+	   "Class": "Warrior",
+	   "Level": 60,
+	   "AAP": 222,
+	   "AP": 220,
+	   "DP": 305,
+	   "GS": 527
+	 },
+	 {
+	   "Name": "Mbkn",
+	   "Class": "Warrior",
+	   "Level": 59,
+	   "AAP": 203,
+	   "AP": 193,
+	   "DP": 289,
+	   "GS": 492
+	 },
+	 {
+	   "Name": "Nicholas_II",
+	   "Class": "Warrior",
+	   "Level": 60,
+	   "AAP": 169,
+	   "AP": 175,
+	   "DP": 281,
+	   "GS": 456
+	 },
+	 {
+	   "Name": "Nacs",
+	   "Class": "Warrior",
+	   "Level": 60,
+	   "AAP": 202,
+	   "AP": 200,
+	   "DP": 291,
+	   "GS": 493
+	 },
+	 {
+	   "Name": "IdiAmin",
+	   "Class": "Warrior",
+	   "Level": 60,
+	   "AAP": 174,
+	   "AP": 172,
+	   "DP": 306,
+	   "GS": 480
+	 },
+	 {
+	   "Name": "Altair",
+	   "Class": "Warrior",
+	   "Level": 60,
+	   "AAP": 231,
+	   "AP": 221,
+	   "DP": 263,
+	   "GS": 494
+	 },
+	 {
+	   "Name": "Luciann",
+	   "Class": "Warrior",
+	   "Level": 59,
+	   "AAP": 201,
+	   "AP": 199,
+	   "DP": 275,
+	   "GS": 476
+	 },
+	 {
+	   "Name": "Zeth955",
+	   "Class": "Warrior",
+	   "Level": 60,
+	   "AAP": 209,
+	   "AP": 179,
+	   "DP": 260,
+	   "GS": 469
+	 },
+	 {
+	   "Name": "Flonkis",
+	   "Class": "Warrior",
+	   "Level": 60,
+	   "AAP": 184,
+	   "AP": 170,
+	   "DP": 280,
+	   "GS": 464
+	 },
+	 {
+	   "Name": "DmikeEX",
+	   "Class": "Warrior",
+	   "Level": 60,
+	   "AAP": 212,
+	   "AP": 210,
+	   "DP": 283,
+	   "GS": 495
+	 },
+	 {
+	   "Name": "Ramsey_Gordon",
+	   "Class": "Warrior",
+	   "Level": 59,
+	   "AAP": 182,
+	   "AP": 160,
+	   "DP": 285,
+	   "GS": 467
+	 },
+	 {
+	   "Name": "Tillmook",
+	   "Class": "Warrior",
+	   "Level": 60,
+	   "AAP": 220,
+	   "AP": 210,
+	   "DP": 300,
+	   "GS": 520
+	 },
+	 {
+	   "Name": "SephyrXD",
+	   "Class": "Warrior",
+	   "Level": 60,
+	   "AAP": 216,
+	   "AP": 214,
+	   "DP": 290,
+	   "GS": 506
+	 },
+	 {
+	   "Name": "Drach",
+	   "Class": "Witch",
+	   "Level": 59,
+	   "AAP": 221,
+	   "AP": 212,
+	   "DP": 313,
+	   "GS": 534
+	 },
+	 {
+	   "Name": "MoriyaSuwako",
+	   "Class": "Witch",
+	   "Level": 61,
+	   "AAP": 220,
+	   "AP": 218,
+	   "DP": 315,
+	   "GS": 535
+	 },
+	 {
+	   "Name": "Elvysia",
+	   "Class": "Witch",
+	   "Level": 61,
+	   "AAP": 231,
+	   "AP": 229,
+	   "DP": 305,
+	   "GS": 536
+	 },
+	 {
+	   "Name": "Whorse",
+	   "Class": "Witch",
+	   "Level": 57,
+	   "AAP": 198,
+	   "AP": 196,
+	   "DP": 249,
+	   "GS": 447
+	 },
+	 {
+	   "Name": "Horizon_Phantasm",
+	   "Class": "Witch",
+	   "Level": 58,
+	   "AAP": 156,
+	   "AP": 156,
+	   "DP": 207,
+	   "GS": 363
+	 },
+	 {
+	   "Name": "NaturalCauses",
+	   "Class": "Witch",
+	   "Level": 56,
+	   "AAP": 185,
+	   "AP": 185,
+	   "DP": 240,
+	   "GS": 425
+	 },
+	 {
+	   "Name": "UrukaChan",
+	   "Class": "Witch",
+	   "Level": 58,
+	   "AAP": 150,
+	   "AP": 120,
+	   "DP": 210,
+	   "GS": 360
+	 },
+	 {
+	   "Name": "Akache",
+	   "Class": "Witch",
+	   "Level": 59,
+	   "AAP": 184,
+	   "AP": 182,
+	   "DP": 292,
+	   "GS": 476
+	 },
+	 {
+	   "Name": "Vesslya",
+	   "Class": "Witch",
+	   "Level": 59,
+	   "AAP": 207,
+	   "AP": 207,
+	   "DP": 258,
+	   "GS": 465
+	 },
+	 {
+	   "Name": "Dibear",
+	   "Class": "Witch",
+	   "Level": 61,
+	   "AAP": 238,
+	   "AP": 220,
+	   "DP": 309,
+	   "GS": 547
+	 },
+	 {
+	   "Name": "Sukime",
+	   "Class": "Witch",
+	   "Level": 60,
+	   "AAP": 218,
+	   "AP": 216,
+	   "DP": 283,
+	   "GS": 501
+	 },
+	 {
+	   "Name": "Living_Death",
+	   "Class": "Witch",
+	   "Level": 58,
+	   "AAP": 206,
+	   "AP": 226,
+	   "DP": 204,
+	   "GS": 430
+	 },
+	 {
+	   "Name": "ImushuI",
+	   "Class": "Witch",
+	   "Level": 58,
+	   "AAP": 207,
+	   "AP": 213,
+	   "DP": 275,
+	   "GS": 488
+	 },
+	 {
+	   "Name": "Kwoni",
+	   "Class": "Witch",
+	   "Level": 61,
+	   "AAP": 240,
+	   "AP": 238,
+	   "DP": 283,
+	   "GS": 523
+	 },
+	 {
+	   "Name": "Suchi_Aiyana",
+	   "Class": "Witch",
+	   "Level": 60,
+	   "AAP": 218,
+	   "AP": 216,
+	   "DP": 268,
+	   "GS": 486
+	 },
+	 {
+	   "Name": "Naeri",
+	   "Class": "Witch",
+	   "Level": 60,
+	   "AAP": 194,
+	   "AP": 198,
+	   "DP": 257,
+	   "GS": 455
+	 },
+	 {
+	   "Name": "Maid_Bot",
+	   "Class": "Witch",
+	   "Level": 61,
+	   "AAP": 232,
+	   "AP": 230,
+	   "DP": 305,
+	   "GS": 537
+	 },
+	 {
+	   "Name": "Spriggy",
+	   "Class": "Witch",
+	   "Level": 58,
+	   "AAP": 198,
+	   "AP": 204,
+	   "DP": 270,
+	   "GS": 474
+	 },
+	 {
+	   "Name": "Omegaz",
+	   "Class": "Witch",
+	   "Level": 60,
+	   "AAP": 250,
+	   "AP": 248,
+	   "DP": 283,
+	   "GS": 533
+	 },
+	 {
+	   "Name": "Shiyii",
+	   "Class": "Witch",
+	   "Level": 60,
+	   "AAP": 198,
+	   "AP": 224,
+	   "DP": 233,
+	   "GS": 457
+	 },
+	 {
+	   "Name": "Croissants",
+	   "Class": "Witch",
+	   "Level": 60,
+	   "AAP": 208,
+	   "AP": 206,
+	   "DP": 263,
+	   "GS": 471
+	 },
+	 {
+	   "Name": "SAINTBRICK",
+	   "Class": "Witch",
+	   "Level": 60,
+	   "AAP": 228,
+	   "AP": 218,
+	   "DP": 263,
+	   "GS": 491
+	 },
+	 {
+	   "Name": "Dualyz",
+	   "Class": "Wizard",
+	   "Level": 61,
+	   "AAP": 231,
+	   "AP": 229,
+	   "DP": 277,
+	   "GS": 508
+	 },
+	 {
+	   "Name": "Caleeal",
+	   "Class": "Wizard",
+	   "Level": 57,
+	   "AAP": 163,
+	   "AP": 169,
+	   "DP": 224,
+	   "GS": 393
+	 },
+	 {
+	   "Name": "TheAbyssal",
+	   "Class": "Wizard",
+	   "Level": 57,
+	   "AAP": 157,
+	   "AP": 142,
+	   "DP": 212,
+	   "GS": 369
+	 },
+	 {
+	   "Name": "Zakr",
+	   "Class": "Wizard",
+	   "Level": 60,
+	   "AAP": 229,
+	   "AP": 219,
+	   "DP": 261,
+	   "GS": 490
+	 },
+	 {
+	   "Name": "HundredBlossom",
+	   "Class": "Wizard",
+	   "Level": 57,
+	   "AAP": 152,
+	   "AP": 142,
+	   "DP": 207,
+	   "GS": 359
+	 },
+	 {
+	   "Name": "itmecornbread",
+	   "Class": "Wizard",
+	   "Level": 59,
+	   "AAP": 235,
+	   "AP": 225,
+	   "DP": 273,
+	   "GS": 508
+	 },
+	 {
+	   "Name": "Thaofrost",
+	   "Class": "Wizard",
+	   "Level": 60,
+	   "AAP": 228,
+	   "AP": 218,
+	   "DP": 268,
+	   "GS": 496
+	 },
+	 {
+	   "Name": "Laoher",
+	   "Class": "Wizard",
+	   "Level": 59,
+	   "AAP": 212,
+	   "AP": 210,
+	   "DP": 211,
+	   "GS": 423
+	 },
+	 {
+	   "Name": "MothugsXD",
+	   "Class": "Wizard",
+	   "Level": 60,
+	   "AAP": 221,
+	   "AP": 221,
+	   "DP": 270,
+	   "GS": 491
+	 },
+	 {
+	   "Name": "Shahzaad",
+	   "Class": "Wizard",
+	   "Level": 60,
+	   "AAP": 183,
+	   "AP": 181,
+	   "DP": 299,
+	   "GS": 482
+	 },
+	 {
+	   "Name": "Jeffernorii",
+	   "Class": "Wizard",
+	   "Level": 60,
+	   "AAP": 224,
+	   "AP": 214,
+	   "DP": 293,
+	   "GS": 517
+	 },
+	 {
+	   "Name": "Caerulus1",
+	   "Class": "Wizard",
+	   "Level": 60,
+	   "AAP": 214,
+	   "AP": 212,
+	   "DP": 313,
+	   "GS": 527
+	 },
+	 {
+	   "Name": "WiidTokes",
+	   "Class": "Wizard",
+	   "Level": 60,
+	   "AAP": 221,
+	   "AP": 211,
+	   "DP": 272,
+	   "GS": 493
+	 },
+	 {
+	   "Name": "Apprentice",
+	   "Class": "Wizard",
+	   "Level": 60,
+	   "AAP": 220,
+	   "AP": 210,
+	   "DP": 292,
+	   "GS": 512
+	 },
+	 {
+	   "Name": "ChickenRaptor",
+	   "Class": "Wizard",
+	   "Level": 60,
+	   "AAP": 228,
+	   "AP": 219,
+	   "DP": 263,
+	   "GS": 491
+	 },
+	 {
+	   "Name": "Dualyz",
+	   "Class": "Wizard",
+	   "Level": 61,
+	   "AAP": 231,
+	   "AP": 229,
+	   "DP": 277,
+	   "GS": 508
+	 },
+	 {
+	   "Name": "Draethel",
+	   "Class": "Wizard",
+	   "Level": 59,
+	   "AAP": 193,
+	   "AP": 191,
+	   "DP": 300,
+	   "GS": 493
+	 },
+	 {
+	   "Name": "Yazule",
+	   "Class": "Wizard",
+	   "Level": 60,
+	   "AAP": 201,
+	   "AP": 215,
+	   "DP": 263,
+	   "GS": 478
+	 }
+	]
